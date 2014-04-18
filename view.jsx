01fb451846f5ce308ad6ff994361602c6a4c9036
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-18 13:09:31 -0700

    Add first element in React and CSS (Boxes)

--- view.jsx
+++ view.jsx
@@ -1,3 +1,52 @@
 /**
 * @jsx React.DOM
 */
+var Category = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentWillMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    console.log(this.state.data)
+    if(this.state.data.sites){
+      var boxes = this.state.data.sites.map(function(box){
+        return <Box name={box.name} description={box.description}></Box>;
+      })
+    }
+    
+    return (
+      <div className="category">
+      <h2>{this.props.title}</h2> 
+        {boxes}
+      </div>
+    );
+  }
+});
+
+var Box = React.createClass({
+  render: function() {
+    return (
+      <div className="box">
+        <h3>{this.props.name}</h3>
+        <p>{this.props.description}</p>
+      </div>
+      );
+  }
+})
+
+
+React.renderComponent(
+  <Category url="data.json" />,
+  document.querySelector('body')
+);
