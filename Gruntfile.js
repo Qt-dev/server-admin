3ddd48f085bac43869d283d06f9b14f03d503957
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-28 15:08:28 +0200

    Add Uglify as a grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,24 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        mangle: true,
+        compress: true
+      },
+      app: {
+        files: {
+          'public/js/output.min.js': ['src/**/*.js', 'src/*.js']
+        }
+      }
+    }
   });
 
+  // JS Uglify task
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['uglify']);
 
 };
