3a4479a43077e2c388c12172bc35d4b481ee7a72
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-29 17:30:21 +0200

    Make the first test pass * Install React * Add React express view engine * Make a basic layout and a hello-world page * Make express use the correct views folder

---
+++ lib/views/index.jsx
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+var React = require('react');
+var DefaultLayout = require('./layouts/default');
+
+var HelloMessage = React.createClass({
+  render: function() {
+    return (
+      <DefaultLayout title={this.props.title}>
+        <div>Hello {this.props.name}</div>
+      </DefaultLayout>
+    );
+  }
+});
+
+module.exports = HelloMessage;
---
+++ lib/views/layouts/default.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+
+var DefaultLayout = React.createClass({
+  render: function() {
+    return (
+      <html>
+        <head><title>{this.props.title}</title></head>
+        <body>{this.props.children}</body>
+      </html>
+    );
+  }
+})
+
+module.exports = DefaultLayout;
