d7fa3bcadfd78845d2a529ba267c0e10520fca47
Quentin Devauchelle <quentin@devauchelle.eu>
2014-04-30 17:58:27 +0200

    Add tests for the json object structure

--- test/routes.js
+++ test/routes.js
@@ -10,10 +10,42 @@ describe('routes', function(){
       .get('/')
       .expect(200, done);
   })
-  it('should have a data.json route', function(done){
-    request(app)
-      .get('/data.json')
-      .expect('Content-Type', /json/)
-      .expect(200,done)
-  })
+  describe('data.json route', function(){
+    it('should exist', function(done){
+      request(app)
+        .get('/data.json')
+        .expect('Content-Type', /json/)
+        .expect(200,done)
+    })
+    describe('rendered object', function(){
+      beforeEach(function(){
+        getData = function(done,test){
+          request(app)
+            .get('/data.json')
+            .end(function(err,res){
+              test(res);
+              done();
+            });
+        }
+      })
+      it('should have a title', function(done){
+        var test = function(res){
+          expect(res.body.title).to.exist;
+        }
+        getData(done,test);
+      })
+      it('should have sites', function(done){
+        var test = function(res){
+          expect(res.body.sites).to.exist;
+        }
+        getData(done,test);
+      })
+      it('should have categories', function(done){
+        var test = function(res){
+          expect(res.body.categories).to.exist;
+        }
+        getData(done,test);
+      })
+    })
+  }) 
 })
