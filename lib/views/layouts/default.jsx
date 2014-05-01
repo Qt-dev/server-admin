2923a291e1d164415e4c7ec3ecdf1473dfa2edd3
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-01 11:06:29 +0200

    Add css and js files to the view

--- lib/views/layouts/default.jsx
+++ lib/views/layouts/default.jsx
@@ -1,15 +1,39 @@
 /** @jsx React.DOM */
 
 var React = require('react');
+var CSS = ['css/style.css'];
+var JS = [
+    'http://fb.me/react-0.10.0.js',
+    'http://code.jquery.com/jquery-1.10.0.min.js',
+    'js/output.min.js'
+    ];
 
 var DefaultLayout = React.createClass({
   render: function() {
     return (
       <html>
-        <head><title>{this.props.title}</title></head>
+        <Head css={CSS} js={JS} />
         <body>{this.props.children}</body>
       </html>
     );
+  }
+})
+
+var Head = React.createClass({
+  render: function(){
+    var styles = this.props.css.map(function (css) {
+      return <link rel="stylesheet" href={css} />;
+    });
+    var javascripts = this.props.js.map(function (js) {
+      return <script src={js} />;
+    });
+    return (
+      <head>
+        <title>{this.props.title}</title>
+        {styles}
+        {javascripts}
+      </head>
+    )
   }
 })
 
