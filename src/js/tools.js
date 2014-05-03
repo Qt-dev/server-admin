78aac16c87e9706e2fba8c0164b9372620700c9d
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-03 21:12:27 +0200

    Refactor the SABNZBD object

--- src/js/tools.js
+++ src/js/tools.js
@@ -30,29 +30,17 @@ var SABNZBD = function(opts){
   this.link = opts.link;
   this.api = opts.api;
   this.paused = false;
-  this.mode = {
-    status: 'qstatus',
-    pause: 'pause',
-    resume: 'resume'
-  };
 };
 
 SABNZBD.prototype = {
   // Tools
-  buildURL: function(mode){
-    return this.link + '/api?mode='+ this.mode[mode] +'&output=json&apikey='+ this.api;
-  },
-  getAPICall: function(url, callback){
-    var inputData = {
-        api: this.api,
-        url: this.link
-      };
-    AJAX.request({url:url, datatype: 'json', data: inputData}, callback, this.unhappyCallback);
-  },
   unhappyCallback: function(status, error){
     console.log(status, error);
   },
-  addReadToCallback: function(callback){
+  buildURL: function(mode){
+    return 'sabnzbd/'+mode;
+  },
+  addReaderToCallback: function(callback){
     var newCallback = function(data){
       this.paused = data.data.paused;
       callback(data);
@@ -60,21 +48,28 @@ SABNZBD.prototype = {
     };
     return newCallback;
   },
+  getAPICall: function(url, callback){
+    AJAX.request({
+        url:url,
+        datatype: 'json',
+        data: {api: this.api, url: this.link}
+      }, callback, this.unhappyCallback);
+  },
+  // Main method - 'The switch'
+  sendRequest: function(mode, happyCallback){
+    var url = this.buildURL(mode);
+    var callback = this.addReaderToCallback(happyCallback);
+    this.getAPICall(url, callback.bind(this));
+  },
   // Interface
   getStatus: function(happyCallback){
-    var url = '/sabnzbd/status';
-    var callback = this.addReadToCallback(happyCallback);
-    this.getAPICall(url, callback.bind(this));
+    this.sendRequest('status', happyCallback);
   },
   pause: function(happyCallback){
-    var url = '/sabnzbd/pause';
-    var callback = this.addReadToCallback(happyCallback);
-    this.getAPICall(url, callback.bind(this));
+    this.sendRequest('pause', happyCallback);
   },
   resume: function(happyCallback){
-    var url = '/sabnzbd/resume';
-    var callback = this.addReadToCallback(happyCallback);
-    this.getAPICall(url, callback.bind(this));
+    this.sendRequest('resume', happyCallback);
   }
 };
 
