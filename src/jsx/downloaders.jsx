4600e84f1ca4bd0389482c81752d9d403f47b844
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-04 15:52:58 +0200

    Refactor JSX for downloader

--- src/jsx/downloaders.jsx
+++ src/jsx/downloaders.jsx
@@ -3,7 +3,6 @@
 */
 var Downloader = (function(){
   var _boxContent = React.createClass({
-    /* REACT */
     render: function(){
       return (
         <div className="boxContent">
@@ -14,10 +13,9 @@ var Downloader = (function(){
           <ul className="history">{this.props.history}< /ul>
         </div>)
     }
-    
   })
 
-  var _statusBox = React.createClass({
+  var _statusLine = React.createClass({
     render: function(){
       if(this.props.data.length !==0){
         var apidata = {
@@ -45,14 +43,14 @@ var Downloader = (function(){
         paused: this.props.paused
       }
     },
-    handlePause: function(e){
-      this.props.toggleCallback(this.props.paused);
+    handleClick: function(e){
+      this.props.callback(this.props.paused);
     },
     render: function(){
       if(this.props.paused){
-        return <a className="bottomButton" href="#" onClick={this.handlePause} >Resume <i className="fa fa-play"></i></a>
+        return <a className="bottomButton" href="#" onClick={this.handleClick} >Resume <i className="fa fa-play"></i></a>
       } else {
-        return <a className="bottomButton" href="#" onClick={this.handlePause} >Pause <i className="fa fa-pause"></i></a>
+        return <a className="bottomButton" href="#" onClick={this.handleClick} >Pause <i className="fa fa-pause"></i></a>
       }
     }
   })
@@ -67,19 +65,6 @@ var Downloader = (function(){
   })
 
   var _box = React.createClass({
-
-      getInitialState: function() {
-        return {data: []};
-      },
-      componentWillMount: function(){
-        var data = {
-          url: this.props.data.content.url,
-          apiKey: this.props.data.content.apiKey
-        }
-        this.apiCaller = new AJAXCaller[this.props.data.type](data);
-        this.apiCaller.getStatus(this.setState.bind(this));
-      },
-
       /* Callbacks */
       togglePause: function(paused){
         if(paused){
@@ -91,39 +76,62 @@ var Downloader = (function(){
       cleanHistory: function(){
         this.apiCaller.cleanHistory(this.refresh);
       },
+
+      /* Tools */
       refresh: function() {
         this.apiCaller.getStatus(this.setState.bind(this));
       },
-      render: function() {
-        var cleanButton = <_cleanButton callback={this.cleanHistory} />;
-
-        if(typeof this.state.data.status !== 'undefined'){
-          var pausedButton = <_pauseToggleButton paused={this.state.data.status.paused} toggleCallback={this.togglePause} />
-        }
-
-        if(this.state.data.length !== 0){
-          var statusBox = <_statusBox data={this.state.data.status} />
-          var queue = this.state.data.queue.map(function(item){
+      buildQueue: function() {
+        var queue = this.state.data.queue.map(function(item){
             return (<li>
                       <span className="title">{item.name}</span> 
                       <span className="percentage">{item.percentage}%</span>
                       <span className="eta">ETA:{item.eta}</span>
                     </li>);
           });
-          var history = this.state.data.history.map(function(item){
+        return queue;
+      },
+      buildHistory: function() {
+        var history = this.state.data.history.map(function(item){
             return (<li>
                       <span className="title">{item.name}</span>
                       <span className="status">{item.status}</span>
                       <span className="failMessage">{item.fail_message}</span>
                     </li>);
-          })
+          });
+        return history;
+      },
+
+      /* REACT */
+      getInitialState: function() {
+        return {data: []};
+      },
+      componentWillMount: function(){
+        var data = {
+          url: this.props.data.content.url,
+          apiKey: this.props.data.content.apiKey
+        }
+        this.apiCaller = new AJAXCaller[this.props.data.type](data);
+        this.apiCaller.getStatus(this.setState.bind(this));
+      },
+      render: function() {
+        var cleanButton = <_cleanButton callback={this.cleanHistory} />;
+
+        if(typeof this.state.data.status !== 'undefined'){
+          var pausedButton = <_pauseToggleButton paused={this.state.data.status.paused} callback={this.togglePause} />
+        }
+
+        if(this.state.data.length !== 0){
+          var statusBox = <_statusLine data={this.state.data.status} />
+          var queue = this.buildQueue();
+          var history = this.buildHistory();
         }
 
         return (
           <div className="box">
             <h3 style={this.props.style}>{this.props.data.name}</h3>
             <_boxContent queue={queue} history={history} type={this.props.data.type} description={this.props.data.content.description} />
-            <BoxFooter buttons={[cleanButton, pausedButton]} link={this.props.data.content.link} statusBox={statusBox} />
+            <BoxFooter buttons={[pausedButton, cleanButton]} link={this.props.data.content.link} statusBox={statusBox} />
           </div>
           );
       }
