069dc8bca918dec4e5b8a8c19161044b99ae2db6
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-04 16:18:52 +0200

    Refactor the router. It is now more RESTful, and has a clean factory method

--- src/js/tools.js
+++ src/js/tools.js
@@ -37,8 +37,8 @@ SABNZBD.prototype = {
   unhappyCallback: function(status, error){
     console.log(status, error);
   },
-  buildURL: function(mode){
-    return 'sabnzbd/'+mode;
+  buildURL: function(){
+    return 'sabnzbd/query';
   },
   addReaderToCallback: function(callback){
     var newCallback = function(data){
@@ -48,18 +48,18 @@ SABNZBD.prototype = {
     };
     return newCallback;
   },
-  getAPICall: function(url, callback){
+  getAPICall: function(url, mode, callback){
     AJAX.request({
         url:url,
         datatype: 'json',
-        data: {key: this.apiKey, url: this.url}
+        data: {key: this.apiKey, url: this.url, action: mode}
       }, callback, this.unhappyCallback);
   },
   // Main method - 'The switch'
   sendRequest: function(mode, happyCallback){
-    var url = this.buildURL(mode);
+    var url = this.buildURL();
     var callback = this.addReaderToCallback(happyCallback);
-    this.getAPICall(url, callback.bind(this));
+    this.getAPICall(url, mode, callback.bind(this));
   },
   // Interface
   getStatus: function(happyCallback){
