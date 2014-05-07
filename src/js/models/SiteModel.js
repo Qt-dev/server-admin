23fe5c4c606a4da5dd7154a68354f94e9e89abe8
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-07 14:07:10 +0200

    Make the model sets its config at initialize, and give it a query function for API Query

--- src/js/models/SiteModel.js
+++ src/js/models/SiteModel.js
@@ -1,3 +1,19 @@
 var Site = Backbone.Model.extend({
-  url: '/sites'
+  url: '/sites',
+  initialize: function(){
+    this.queryUrl = '/'+this.get('type')+'/query';
+    this.config = this.get('config');
+  },
+  query: function(action, callback){
+    var config = this.config;
+    config.action = action;
+
+    var data = {
+      url: this.queryUrl,
+      datatype: 'json',
+      data: config
+    };
+
+    AJAX.request(data, callback, function(error){console.log(error);});
+  }
 });
