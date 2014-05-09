30794ef4e201634107067a4b285e5e12a006c89d
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-09 18:11:46 +0200

    Add a pause toggle button to transmission.

--- src/jsx/boxes/transmission.jsx
+++ src/jsx/boxes/transmission.jsx
@@ -5,6 +5,7 @@
 var TransmissionContentBox = React.createClass({
   render: function() {
     if(this.props.data){
+      console.log(this.props);
       var statusText = { 
           0: 'STOPPED',
           1: 'CHECK_WAIT',
@@ -15,7 +16,6 @@ var TransmissionContentBox = React.createClass({
           6: 'SEED',
           7: 'ISOLATED' 
         };
-      console.log(this.props);
       var others = this.props.data.others.map(function(item){
         var status = statusText[item.status];
         return  <li className="item" key={item.id}>
@@ -51,12 +51,28 @@ var TransmissionContentBox = React.createClass({
 })
 
 var TransmissionButtons = (function(){
-  
+  var _pauseToggleButton = React.createClass({
+    render: function(){
+      if(this.props.paused){
+        return <a className="bottomButton" href="#" onClick={this.props.callback} >Resume</a>
+      } else {
+        return <a className="bottomButton" href="#" onClick={this.props.callback} >Pause</a>
+      }
+    }
+  })
+
   return React.createClass({
-    
+    handlePauseToggle: function(e){
+      e.preventDefault();
+      this.props.model.query('pauseToggle',this.props.refresh,{paused: this.props.data.status.paused});
+    },
     render: function() {
+      if(this.props.data){
+        var paused = this.props.data.status.paused;
+      } 
       return (
         <div className="customButtons">
+          <_pauseToggleButton paused={paused} callback={this.handlePauseToggle} />
         </div>
       );
     }
