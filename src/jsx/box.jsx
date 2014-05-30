b25c39392b42ec58a5bce1e3c8d38cf2f14e3c72
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-30 13:57:12 +0200

    Make the item list a bit more object oriented. It now has a closed boolean, and the icon class is stocked in the state.

--- src/jsx/box.jsx
+++ src/jsx/box.jsx
@@ -78,23 +78,35 @@ var ItemList = React.createClass({
     return {
       style: {
         display: "none"
-      }
+      },
+      closed: true,
+      iconClass: "fa fa-angle-down"
     }
   },
   toggleList: function(){
-    if(this.state.style.display === "none"){
-      this.setState({
-        style: {
-          display: "inline"
-        }
-    })
+    if(this.state.closed){
+      this.openList();
     } else {
-      this.setState({
-        style: {
-          display: "none"
-        }
-      })
+      this.closeList();
     }
+  },
+  openList: function(){
+    this.setState({
+      style: {
+        display: "inline"
+      },
+      closed: false,
+      iconClass: "fa fa-angle-up"
+    })
+  },
+  closeList: function(){
+    this.setState({
+      style: {
+        display: "none"
+      },
+      closed: true,
+      iconClass: "fa fa-angle-down"
+    })
   },
   buildList: function(){
     var list = this.props.items.map(function(item){
@@ -111,7 +123,7 @@ var ItemList = React.createClass({
   render: function(){
     return (
       <div className="itemList row">
-        <h4 onTouchEnd={this.toggleList} onClick={this.toggleList} >{this.props.title}({this.props.items.length}) <i className="fa fa-plus-square"></i></h4>
+        <h4 onTouchEnd={this.toggleList} onClick={this.toggleList} >{this.props.title}({this.props.items.length}) <i className={this.state.iconClass}></i></h4>
         <ul style={this.state.style}>{this.buildList()}</ul>
       </div>
     )
