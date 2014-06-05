cf8221a98b2cf28896bf8d719dd24410539ac3e8
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-05 14:41:10 +0200

    Move the ul outside of the "statusBox" in sabnzbd box

--- src/jsx/boxes/sabnzbd.jsx
+++ src/jsx/boxes/sabnzbd.jsx
@@ -91,9 +91,11 @@ var SABStatusBar = React.createClass({
       }
     }
     return(
-      <ul className="statusBox">
-      {status}
-      </ul>
+      <div className="statusBox">
+        <ul>
+        {status}
+        </ul>
+      </div>
       )
   }
 })
