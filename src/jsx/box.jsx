3c65fedf46b103ce6d22a894087d3702198cfe86
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-05 15:07:54 +0200

    Separate boxHeader from the H3

--- src/jsx/box.jsx
+++ src/jsx/box.jsx
@@ -30,7 +30,7 @@ var Box = React.createClass({
 
     return (
       <div className="box">
-        <h3 className="boxHeader" style={style}>{this.props.site.get('name')}</h3>
+        <div className="boxHeader" style={style}><h3>{this.props.site.get('name')}</h3></div>
         <BoxContent contentBox={contentBox} statusBar={statusBar} description={this.props.site.get('description')} />
         <BoxFooter buttons={buttons} link={this.props.site.config.url} />
       </div>
