6401f22b884b9cc349822f1255de81c4b9863fb2
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-07 18:49:25 +0200

    Make the apis controller call the api with a callback

--- app/controllers/apis.js
+++ app/controllers/apis.js
@@ -3,5 +3,8 @@ var apiDir = '../../lib/apis/'
 exports.query = function(req, res){
     var app = req.query.app;
     var api = require(apiDir + app);
-    res.json(api.query(req))
+    var callback = function(response){
+      res.json(response)
+    }
+    api.query(req, callback);
   }
