145b402206641027dd327ee9e352b48627dedd44
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-07 14:34:22 +0200

    Make a test suite for the categories controller

---
+++ test/controllers/categories.js
@@ -0,0 +1,35 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var categoriesController = require('../../app/controllers/categories');
+
+describe('The Categories controller', function(){
+  before(function(){
+    request = {};
+    response = {};
+  })
+  describe('the index route', function(){
+    it('should exist', function(){
+      expect(categoriesController.index).to.exist;
+    })
+    it('should render a json array', function(){
+      response.json = sinon.spy();
+      categoriesController.index(request, response);
+
+      expect(response.json.called).to.be.true;
+      expect(response.json.args[0][0] instanceof Array).to.be.true
+    })
+  })
+  describe('the show route', function(){
+    it('should exist', function(){
+      expect(categoriesController.show).to.exist;
+    })
+    it('should render a json object', function(){
+      request.params = { id: 0 }
+      response.json = sinon.spy();
+      categoriesController.show(request, response);
+
+      expect(response.json.called).to.be.true;
+      expect(response.json.args[0][0] instanceof Array).to.be.false
+    })
+  })
+})
