eccc95b15837d5020c5412b22d3f154321f11dc3
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-07 16:06:23 +0200

    Make tests for the router. We use proxyquire

--- test/helper.js
+++ test/helper.js
@@ -1,5 +1,6 @@
 global.expect = require('chai').expect;
 global.sinon = require('sinon');
+global.proxyquire = require('proxyquire');
 
 before(function(){
   request = {};
--- test/routes.js
+++ test/routes.js
@@ -1,55 +1,33 @@
-var request = require('supertest')
-  , express = require('express')
-  , expect = require('chai').expect;
+var request = require('supertest');
+var express = require('express');
 
-var app = require('../lib/app');
 
-describe('routes', function(){
-  describe('the index router', function(){
-    describe('the main route', function(){
-      it('should have an index route', function(done){
-        request(app)
-          .get('/')
-          .expect(200, done);
-      })
-    })
-    describe('data.json route', function(){
-      it('should exist', function(done){
-        request(app)
-          .get('/data.json')
-          .expect('Content-Type', /json/)
-          .expect(200,done)
-      })
-      describe('rendered object', function(){
-        beforeEach(function(){
-          getData = function(done,test){
-            request(app)
-              .get('/data.json')
-              .end(function(err,res){
-                test(res);
-                done();
-              });
-          }
-        })
-        it('should have a title', function(done){
-          var test = function(res){
-            expect(res.body.title).to.exist;
-          }
-          getData(done,test);
-        })
-        it('should have sites', function(done){
-          var test = function(res){
-            expect(res.body.sites).to.exist;
-          }
-          getData(done,test);
-        })
-        it('should have categories', function(done){
-          var test = function(res){
-            expect(res.body.categories).to.exist;
-          }
-          getData(done,test);
-        })
-      })
-    }) 
+describe('The routes', function(){
+  before(function(){
+
+    var sendResponse = function(req,res){
+      res.send(200,{ok:true})
+    }
+    pagesMock = {
+      index: sendResponse
+    }
+
+
+
+    var router = proxyquire('../lib/routes', {
+      '../app/controllers/pages': pagesMock
+    });
+
+    app = express();
+    app.use('/', router);
   })
+  it('should route "/" to pages.index', function(done){
+    request(app).get('/')
+            .expect(200)
+            .end(function(err,res){
+              expect(res.ok).to.be.true;
+              done();
+            })
+  })
+
 })
