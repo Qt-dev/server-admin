39fe35e23483254b15babc355589a45a12531ace
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-07 16:45:51 +0200

    Add tests for the other routes

--- test/routes.js
+++ test/routes.js
@@ -3,31 +3,71 @@ var express = require('express');
 
 
 describe('The routes', function(){
-  before(function(){
-
+  function setMocksUp(){
     var sendResponse = function(req,res){
-      res.send(200,{ok:true})
+      res.send(200,{ok:true, params: req.params})
     }
     pagesMock = {
       index: sendResponse
     }
+    sitesMock = {
+      index: sendResponse
+    }
+    categoriesMock = {
+      index: sendResponse,
+      show: sendResponse
+    }
+  }
 
-
-
+  before(function(){
+    setMocksUp();
     var router = proxyquire('../lib/routes', {
-      '../app/controllers/pages': pagesMock
+      '../app/controllers/pages': pagesMock,
+      '../app/controllers/sites': sitesMock,
+      '../app/controllers/categories': categoriesMock
     });
 
     app = express();
     app.use('/', router);
   })
-  it('should route "/" to pages.index', function(done){
-    request(app).get('/')
-            .expect(200)
-            .end(function(err,res){
-              expect(res.ok).to.be.true;
-              done();
-            })
+  describe('pages', function(){
+    it('should route "/" to pages.index', function(done){
+      request(app).get('/')
+              .expect(200)
+              .end(function(err,res){
+                expect(res.body.ok).to.be.true;
+                done();
+              })
+    })
+  })
+  describe('the sites', function(){
+    it('should route "/sites" to sites.index', function(done){
+      request(app).get('/sites')
+              .expect(200)
+              .end(function(err,res){
+                expect(res.body.ok).to.be.true;
+                done();
+              })
+    })
+  })
+  describe('the sites', function(){
+    it('should route "/categories" to sites.index', function(done){
+      request(app).get('/categories')
+              .expect(200)
+              .end(function(err,res){
+                expect(res.body.ok).to.be.true;
+                done();
+              })
+    })
+    it('should route "/category/id" to sites.index', function(done){
+      request(app).get('/categories/0')
+              .expect(200)
+              .end(function(err,res){
+                expect(res.body.params.id).to.equal('0');
+                expect(res.body.ok).to.be.true;
+                done();
+              })
+    })
   })
 
 })
