1634dc7e8feae0f73604e7ee6e080cb673517eb2
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-11 16:36:17 +0200

    Add requirements on the category model to make tests pass

--- app/models/category.js
+++ app/models/category.js
@@ -1,15 +1,21 @@
-var Category = function(){
+var Color = require('./color');
+var Category = function(Color){
   var Schema = mongoose.Schema;
   var categorySchema = new Schema({
     idName: {type: String},
     title: {type: String},
-    color: [mongoose.model('Color')]
+    color: {type: Color}
   })
+  categorySchema.path('title').required(true, "The category's name must not be empty");
+  categorySchema.path('idName').required(true, "The category's idName must not be empty");
+  categorySchema.path('color').required(true, "You need a color for each category");
+  
+
   var _model = mongoose.model('Category', categorySchema);
   return {
     schema: categorySchema,
     model: _model
   }
-}()
+}(Color)
 
 module.exports = Category;
