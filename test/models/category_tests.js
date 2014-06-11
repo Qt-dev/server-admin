173f80a2c032a4e05e6def069dea892f0c64374a
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-11 16:38:37 +0200

    A little spacing never hurts, right?

--- test/models/category_tests.js
+++ test/models/category_tests.js
@@ -15,19 +15,23 @@ describe('The Category model', function(){
       done();
     });
   })
+
   after(function(done){
     mongoose.model('Color').find().remove();
     mongoose.model('Category').find().remove();
     done();
   })
 
+
   it('should exist', function(){
     expect(Category).to.exist;
   })
+
   it('should have a model and a schema', function(){
     expect(Category.model).to.exist;
     expect(Category.schema).to.exist;
   })
+
   it('should allow us to create a Category with all the needed data', function(done){
     var params = {idName:'test', title:'test', color: color}
     mongoose.model('Category').create(params, function(error, category){
@@ -37,7 +41,10 @@ describe('The Category model', function(){
       done();
     })
   })
+
+
   describe('validations', function(){
+    
     it('should not allow us to create a Category without an idname', function(done){
       var params = {title: 'tester', color: color};
       mongoose.model('Category').create(params, function(error, category){
@@ -45,6 +52,7 @@ describe('The Category model', function(){
         done();
       })
     })
+
     it('should not allow us to create a Category without a color', function(done){
       var params = {idName: 'test', title: 'tester'};
       mongoose.model('Category').create(params, function(error, category){
