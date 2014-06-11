f50614050b1fa5203496485b053cfd18ea0b9f91
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-11 19:20:58 +0200

    Add a test on the global Site structure

--- test/models/site_tests.js
+++ test/models/site_tests.js
@@ -19,10 +19,27 @@ describe('The Site model', function(){
     expect(Site.schema).to.exist;
   })
 
+  it('should allow us to create a Site with all the needed data', function(done){
+    var params = {
+      title: 'test',
+      type: 'test-site',
+      description: 'testing app',
+      config: {url: 'http://test.com', apiKey: '111222'},
+      category: category
+    }
+    mongoose.model('Site').create(params, function(error, site){
+      expect(error).to.equal(null);
+      expect(site.title).to.equal(params.title);
+      expect(site.type).to.equal(params.type);
+      expect(site.description).to.equal(params.description);
+      expect(site.config).to.equal(params.config);
+      expect(site.category).to.equal(params.category);
+
+      done();
+    })
+  })
+
   it('should have a findAll that returns an array of the sites')
-
-  it('should allow us to create a Site with all the needed data')
-
 
   describe('validations', function(){
     beforeEach(function(done){
