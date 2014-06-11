18fd4acc63c810c05e9d69da010fa309888e4afb
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-11 13:19:59 +0200

    Add db load/unload with mongoose and tungus in the helper

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,35 @@
 global.expect = require('chai').expect;
 global.sinon = require('sinon');
 global.proxyquire = require('proxyquire');
+global.tungus = require('tungus')
+global.mongoose = require('mongoose');
+var fs = require('fs');
+var dbFolder = __dirname + '/db'
 
 before(function(){
   request = {};
   response = {};
+
+  prepareDb(dbFolder);
+  mongoose.connect('tingodb://'+dbFolder)
 })
+
+after(function(){
+  cleanDb(dbFolder);
+})
+
+function prepareDb(folder){
+  if(!fs.existsSync(folder)){ 
+    fs.mkdir(folder) 
+  } else { 
+    console.log('No need to create db, it already exists');
+  }
+}
+
+function cleanDb(folder){
+  fs.readdirSync(folder).forEach(function(file,index){
+      var curPath = folder + "/" + file;
+      fs.unlinkSync(curPath);
+    });
+  fs.rmdir(folder)
+}
