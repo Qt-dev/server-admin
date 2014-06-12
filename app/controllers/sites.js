18bb947b6539b20c3fbe2aeb08350476039a9501
Quentin Devauchelle <quentin@devauchelle.eu>
2014-06-12 13:31:00 +0200

    Change the sites controller to use our new model

--- app/controllers/sites.js
+++ app/controllers/sites.js
@@ -1,5 +1,12 @@
 var data = require('../../data.json');
+var Site = require('../models/site');
 
 exports.index = function(req, res){
-  res.json(data.sites);
+  Site.findAll(function(error, sites){
+    if(error){
+      res.json({error: error})
+    } else {
+      res.json(sites)
+    }
+  })
 };
